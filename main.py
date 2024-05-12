1376e8a49abec750851c39ea0c7f3f63806eeddb
bybuteng888 <bybuteng@gmail.com>
2024-05-12 22:11:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ def main():
     #创建任务对象
     _aiqiyi=aiqiyi()
     contents=_aiqiyi.start()
-    #创建信息推送对象
-    _push = push()
-    #钉钉推送
-    try:
-        _push.start(contents)
-    except:
-        print('[+]推送参数未填写或推送脚本出错')
+    
 if __name__ == '__main__':
     main()
