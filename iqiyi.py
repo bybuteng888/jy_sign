d27ae2fdfd96afbbbfe5ae569cb1df596ad63bfc
bybuteng888 <bybuteng@gmail.com>
2024-05-14 18:39:29 +0800

    Update iqiyi.py

--- iqiyi.py
+++ iqiyi.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-# iqiyi.py
+# aiqiyi.py
 
 import requests
 import os
-
-def sign_in_with_cookie(jy_cookie):
+jy_cookie = os.environ["jy_cookie"]
+def jy_cookie(jy_cookie):
     URL="https://bbs.125.la/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
