d638063628a6282d846114e1b72758eb6187ee0f
bybuteng888 <bybuteng@gmail.com>
2024-05-14 18:20:14 +0800

    Update iqiyi.py

--- iqiyi.py
+++ iqiyi.py
@@ -1,30 +1,30 @@
-# -*- coding: utf-8 -*-
+*# -*- coding: utf-8 -*-
 
-
+# aiqiyi.py
 
 import requests
 import os
 
-jy_cookie = os.environ["jy_cookie"]
+def sign_in_with_cookie(jy_cookie):
+    URL="https://bbs.125.la/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+        'cookie': jy_cookie,
+        'Referer': 'https://bbs.125.la/'
+    }
 
-URL="https://bbs.125.la/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'cookie': jy_cookie,
-    'Referer': 'https://bbs.125.la/'
-
-}
-
-data = {
-    "formhash": "9c12567f",
-    "submit": "1",
-    "targerurl": "",
-    "todaysay": "",
-    "qdxq": "fd"
-}
+    data = {
+        "formhash": "9c12567f",
+        "submit": "1",
+        "targerurl": "",
+        "todaysay": "",
+        "qdxq": "fd"
+    }
      
-response = requests.post(URL, headers=headers, data=data)
+    response = requests.post(URL, headers=headers, data=data)
 
-print(response.text)
+    return response.text
+
+
 
 
