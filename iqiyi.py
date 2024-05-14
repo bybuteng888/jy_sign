e91bdf4fd678e0f588761736cc3e2a790acb85bd
bybuteng888 <bybuteng@gmail.com>
2024-05-14 19:05:33 +0800

    Update iqiyi.py

--- iqiyi.py
+++ iqiyi.py
@@ -1,30 +1,44 @@
 # -*- coding: utf-8 -*-
 
-# aiqiyi.py
-
+import json
+import os
+import re
+import time
+import requests
 import requests
 import os
+
+
 jy_cookie = os.environ["jy_cookie"]
-def jy_cookie(jy_cookie):
-    URL="https://bbs.125.la/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1"
-    headers = {
+class aiqiyi:
+def user_information(self):
+        URL="https://bbs.125.la/plugin.php?id=dsu_paulsign:sign&operation=qiandao&infloat=1"
+headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
         'cookie': jy_cookie,
         'Referer': 'https://bbs.125.la/'
     }
-
-    data = {
+data = {
         "formhash": "9c12567f",
         "submit": "1",
         "targerurl": "",
         "todaysay": "",
         "qdxq": "fd"
     }
-     
-    response = requests.post(URL, headers=headers, data=data)
+response = requests.post(URL, headers=headers, data=data)
 
-    return response.text
+if response.json()["credit"]==""
+    try:
+                msg = "签到成功"
+    except:
+                msg = response.json()
+    else:
+        # print("（iqy）签到错误", res.content.decode())
+                msg = response.json()
+print(msg)
+return msg
 
+    return msg
 
 
 
