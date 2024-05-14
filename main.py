175af9a8d0d1769aa0b478717e8f11623c74bde6
bybuteng888 <bybuteng@gmail.com>
2024-05-14 18:27:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from iqiyi import aiqiyi
+
 
 
 '''脚本入口'''
 def main():
     #创建任务对象
-    _aiqiyi=aiqiyi()
-    contents=_aiqiyi.start()
+ 
+    contents=_iqiyi.start()
     
 if __name__ == '__main__':
     main()
